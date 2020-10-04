cfbd7a5e2d1131b1317b0b0f61b801ff4d7f4d93
Alberto <albduranlopez@gmail.com>
2020-10-04 20:56:09 +0200

    Previous function to mergesort

--- algorithms.py
+++ algorithms.py
@@ -39,6 +39,43 @@ def insertion(nums):
         
         
         
+        
+
+def merge(izq, dcha):
+    if len(izq) == 0:
+        return dcha
+
+    if len(dcha) == 0:
+        return izq
+
+    dev = []
+    index_izq = index_dcha = 0
+
+   
+    while len(dev) < len(izq) + len(dcha):
+       
+        if izq[index_izq] <= dcha[index_dcha]:
+            dev.append(izq[index_izq])
+            index_izq += 1
+        else:
+            dev.append(dcha[index_dcha])
+            index_dcha += 1
+
+       
+        if index_dcha == len(dcha):
+            dev += izq[index_izq:]
+            break
+
+        if index_izq == len(izq):
+            dev += dcha[index_dcha:]
+            break
+
+    return dev
+
+
+
+
+
 #ej2
 if __name__=="__main__":
 
